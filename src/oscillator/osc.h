614acd480be2018d1f603fca153425a69fca3aca
Fief Heol <heol.fief@insa-rennes.fr>
2019-02-10 10:48:48 +0100

    Add osc_fill_buffer function implementation

--- src/oscillator/osc.h
+++ src/oscillator/osc.h
@@ -1,9 +1,9 @@
 /**
- * \file osc.c
+ * \file osc.h
  * \brief Oscillator data structure and function definition.
  *
- * Programme de test pour l'objet de gestion des chaînes de caractères Str_t.
  *
+ * Here are defined the data structure of the oscillator, and the related functions
  */
 
 #ifndef AUDIO_SYNTH_OSC_H
@@ -26,13 +26,29 @@ typedef enum{
  * \struct Oscillator
  * \brief define an oscillator
  *
- * An oscillator is defined with : wave, frequency, amplitude, detune
+ * An oscillator is defined with : wave, frequency, amplitude, detune and dutycycle
  */
 typedef struct{
     Waveform wave;       /*!<the waveform type */
-    Uint16 freq;         /*!<the frequency */
-    Uint16 amp;          /*!<the amplitude */
-    Sint8 detune;        /*!<the detune in number of notes */
+    Uint16   freq;       /*!<the frequency */
+    Uint16   amp;        /*!<the amplitude */
+    Sint8    detune;     /*!<the detune in number of notes */
+    Sint8    duty;       /*!<the dutycyle (if available for the selected waveform */
 }Oscillator;
+
+
+/**
+ * \fn Uint8 osc_fill_buffer(const Oscillator *osc, Sint16 *buffer, Uint16 buffer_length, Uint64 sample_rate)
+ * \brief Function to fill audio buffer with oscillator wave
+ *
+ * \param osc The oscillator structure
+ * \param buffer The audio buffer to write audio samples to
+ * \param buffer_length The size of the audio buffer (number of audio samples in the buffer)
+ * \param sample_rate The sample rate of the system
+ *
+ * \return 0 if everything went OK, -1 otherwise
+ */
+Uint8 osc_fill_buffer(const Oscillator *osc, Sint16 *buffer, Uint16 buffer_length, Uint64 sample_rate);
+
 
 #endif //AUDIO_SYNTH_OSC_H
