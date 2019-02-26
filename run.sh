cfc8d7824d18b186ec5a1b169ea8f531be9b0cf3
Fief Heol <heol.fief@insa-rennes.fr>
2019-02-26 20:09:21 +0100

    Separate build and run/test cripts

--- run.sh
+++ run.sh
@@ -3,14 +3,6 @@
 BLUE='\033[1;36m'
 NC='\033[0m' # No Color
 
-echo -e "${BLUE}Building :${NC}"
-rm -rf build/run && mkdir -p build/run
-cd build/run
-cmake ../..
-make
-cd ../..
-echo -e "${BLUE}\nDone${NC}"
-
 echo -e "${BLUE}\nExecuting program :${NC}"
 bin/audio_synth
 echo -e "${BLUE}\nDone${NC}"
