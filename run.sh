ead3a3f586efa20d0838a63a3e090d4da449e787
Fief Heol <heol.fief@insa-rennes.fr>
2019-02-26 16:38:36 +0100

    Fix bug: mkdir add -p option

--- run.sh
+++ run.sh
@@ -4,7 +4,7 @@ BLUE='\033[1;36m'
 NC='\033[0m' # No Color
 
 echo -e "${BLUE}Building :${NC}"
-rm -rf build/run && mkdir build/run
+rm -rf build/run && mkdir -p build/run
 cd build/run
 cmake ../..
 make
--- test.sh
+++ test.sh
@@ -4,13 +4,14 @@ BLUE='\033[1;36m'
 NC='\033[0m' # No Color
 
 echo -e "${BLUE}Building :${NC}"
-rm -rf build/test && mkdir build/test
+rm -rf build/test && mkdir -p build/test
 cd build/test
 cmake ../..
 make
+pwd
 cd ../..
 echo -e "${BLUE}\nDone${NC}"
-
+pwd
 echo -e "${BLUE}\nExecuting test program :${NC}"
 ctest $1
 rm Testing -r
