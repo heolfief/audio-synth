12d122125d90edc66315ae9c12b83f398290df99
Colombel Vincent <vcolombe@insa-rennes.fr>
2019-03-10 14:40:04 +0100

    add commentary

--- src/fichier/fichier.c
+++ src/fichier/fichier.c
@@ -16,7 +16,7 @@
 
 FILE * openFile (char * nom, char * mode, TypeRetour t){
     FILE * fichier;
-    if ((fichier = fopen(nom,"r")) == (FILE *) NULL){
+    if ((fichier = fopen(nom,"r")) == (FILE *) NULL){ //verify that File exist otherwise error
         printf("open file error");
         return fichier;
     }
@@ -31,7 +31,7 @@ int closeFile(FILE * fichier){
 
 unsigned char * BlockFileReader (FILE * fichier, u_int32_t nombre){
     unsigned char * buffer;
-    if ((buffer = (unsigned char *) malloc(sizeof(unsigned char)*nombre)) == NULL){
+    if ((buffer = (unsigned char *) malloc(sizeof(unsigned char)*nombre)) == NULL){ // verify the allocation
         printf("memory allocation error");
         exit (568);
     }
