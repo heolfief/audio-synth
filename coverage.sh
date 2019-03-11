26fbf8a269fe48f58003f42168d1da9ab9855046
Fief Heol <heol.fief@insa-rennes.fr>
2019-03-11 21:36:26 +0100

    Remove test files from coverage

--- coverage.sh
+++ coverage.sh
@@ -4,9 +4,10 @@ BLUE='\033[1;36m'
 NC='\033[0m' # No Color
 
 echo -e "${BLUE}\nExecuting code coverage :${NC}"
-bin/audio_synth_test > /dev/null 2>&1
+bin/audio_synth_test > /dev/null 2>&1 #mute output
 lcov --capture --directory build/run --output-file build/run/coverage.info
-genhtml build/run/coverage.info --output-directory doc/coverage
+lcov --remove build/run/coverage.info $PWD'/test/*' -o build/run/coverage_clean.info
+genhtml build/run/coverage_clean.info --output-directory doc/coverage
 echo -e "${BLUE}\nDone${NC}"
 echo -e "Please open doc/coverage/index.html to see result\n"
 echo -e "Opening doc/coverage/index.html with default browser\n"
