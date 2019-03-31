e33819d732ad6c773bb51df5d4cdc2e73345d468
Fief Heol <heol.fief@insa-rennes.fr>
2019-03-31 18:26:03 +0200

    Fix CI issue : path to preset file was wrong when bin was run by srcipt

--- test.sh
+++ test.sh
@@ -4,6 +4,10 @@ BLUE='\033[1;36m'
 NC='\033[0m' # No Color
 
 echo -e "${BLUE}\nExecuting test program :${NC}"
+cp CTestTestfile.cmake bin
+cd bin
 ctest --output-on-failure $1
-exit $?
+e=$?
 echo -e "${BLUE}\nDone${NC}"
+rm -rf Testing CTestTestfile.cmake
+exit $e
