8e232879d8dcd3542f242e69d881a68e0376ea32
Colombel Vincent <vcolombe@insa-rennes.fr>
2019-04-02 09:17:34 +0200

    basic commentary

--- src/Listmidi/listMidi.h
+++ src/Listmidi/listMidi.h
@@ -26,72 +26,256 @@ typedef struct {
     midiList * last;		/*!< L'adresse du dernier élément de la liste */
 }list;
 
+/**
+ * \fn midiList * newList(void * ptr, nodeList * next)
+ * \brief Function to open a target File
+ *
+ * \param name Name of the target file
+ * \param mode Opening method : r+: reading, w+: writting ...
+ * \param t TypeRetour kind of return
+ *
+ * \return the target File open
+ */
 
+midiList * newList(void * ptr, nodeList * next);
 
-midiList * newList(void * ptr, nodeList * next, nodeList * previous);
-
-
+/**
+ * \fn void initList(list * l)
+ * \brief Function to open a target File
+ *
+ * \param name Name of the target file
+ * \param mode Opening method : r+: reading, w+: writting ...
+ * \param t TypeRetour kind of return
+ *
+ * \return the target File open
+ */
 void initList(list * l);
 
-
+/**
+ * \fn void printList(list * l)
+ * \brief Function to open a target File
+ *
+ * \param name Name of the target file
+ * \param mode Opening method : r+: reading, w+: writting ...
+ * \param t TypeRetour kind of return
+ *
+ * \return the target File open
+ */
 void printList(list * l);
-
+/**
+ * \fn int empty(list * l)
+ * \brief Function to open a target File
+ *
+ * \param name Name of the target file
+ * \param mode Opening method : r+: reading, w+: writting ...
+ * \param t TypeRetour kind of return
+ *
+ * \return the target File open
+ */
 int empty(list * l);
-
+/**
+ * \fn int oneElement(list * l)
+ * \brief Function to open a target File
+ *
+ * \param name Name of the target file
+ * \param mode Opening method : r+: reading, w+: writting ...
+ * \param t TypeRetour kind of return
+ *
+ * \return the target File open
+ */
 
 int oneElement(list * l);
-
+/**
+ * \fn int count(list* l)
+ * \brief Function to open a target File
+ *
+ * \param name Name of the target file
+ * \param mode Opening method : r+: reading, w+: writting ...
+ * \param t TypeRetour kind of return
+ *
+ * \return the target File open
+ */
 int count(list* l);
 
-
+/**
+ * \fn int first(list * l)
+ * \brief Function to open a target File
+ *
+ * \param name Name of the target file
+ * \param mode Opening method : r+: reading, w+: writting ...
+ * \param t TypeRetour kind of return
+ *
+ * \return the target File open
+ */
 int first(list * l);
 
-
+/**
+ * \fn int last(list * l)
+ * \brief Function to open a target File
+ *
+ * \param name Name of the target file
+ * \param mode Opening method : r+: reading, w+: writting ...
+ * \param t TypeRetour kind of return
+ *
+ * \return the target File open
+ */
 int last(list * l);
 
-
+/**
+ * \fn int outOfList(list * l)
+ * \brief Function to open a target File
+ *
+ * \param name Name of the target file
+ * \param mode Opening method : r+: reading, w+: writting ...
+ * \param t TypeRetour kind of return
+ *
+ * \return the target File open
+ */
 int outOfList(list * l);
-
+/**
+ * \fn void setOnFirst(list * l)
+ * \brief Function to open a target File
+ *
+ * \param name Name of the target file
+ * \param mode Opening method : r+: reading, w+: writting ...
+ * \param t TypeRetour kind of return
+ *
+ * \return the target File open
+ */
 void setOnFirst(list * l);
-
+/**
+ * \fn void setOnLast(list * l)
+ * \brief Function to open a target File
+ *
+ * \param name Name of the target file
+ * \param mode Opening method : r+: reading, w+: writting ...
+ * \param t TypeRetour kind of return
+ *
+ * \return the target File open
+ */
 void setOnLast(list * l);
-
+/**
+ * \fn void next(list * l)
+ * \brief Function to open a target File
+ *
+ * \param name Name of the target file
+ * \param mode Opening method : r+: reading, w+: writting ...
+ * \param t TypeRetour kind of return
+ *
+ * \return the target File open
+ */
 void next(list * l);
-
-void previous(list * l);
-
+/**
+ * \fn void* getCurrent(list * l)
+ * \brief Function to open a target File
+ *
+ * \param name Name of the target file
+ * \param mode Opening method : r+: reading, w+: writting ...
+ * \param t TypeRetour kind of return
+ *
+ * \return the target File open
+ */
 
 void* getCurrent(list * l);
 
-
+/**
+ * \fn int insertFirst(list * l, void* data)
+ * \brief Function to open a target File
+ *
+ * \param name Name of the target file
+ * \param mode Opening method : r+: reading, w+: writting ...
+ * \param t TypeRetour kind of return
+ *
+ * \return the target File open
+ */
 int insertFirst(list * l, void* data);
-
+/**
+ * \fn int deleteFirst(list * l, void** data)
+ * \brief Function to open a target File
+ *
+ * \param name Name of the target file
+ * \param mode Opening method : r+: reading, w+: writting ...
+ * \param t TypeRetour kind of return
+ *
+ * \return the target File open
+ */
 
 int deleteFirst(list * l, void** data);
-
+/**
+ * \fn void freeNodeList(nodeList * n)
+ * \brief Function to open a target File
+ *
+ * \param name Name of the target file
+ * \param mode Opening method : r+: reading, w+: writting ...
+ * \param t TypeRetour kind of return
+ *
+ * \return the target File open
+ */
 
 void freeNodeList(nodeList * n);
-
+/**
+ * \fn void freeList(list * l)
+ * \brief Function to open a target File
+ *
+ * \param name Name of the target file
+ * \param mode Opening method : r+: reading, w+: writting ...
+ * \param t TypeRetour kind of return
+ *
+ * \return the target File open
+ */
 
 void freeList(list * l);
-
+/**
+ * \fn int insertLast(list * l, void* data)
+ * \brief Function to open a target File
+ *
+ * \param name Name of the target file
+ * \param mode Opening method : r+: reading, w+: writting ...
+ * \param t TypeRetour kind of return
+ *
+ * \return the target File open
+ */
 
 int insertLast(list * l, void* data);
 
-
+/**
+ * \fn int deleteLast(list * l, void** data)
+ * \brief Function to open a target File
+ *
+ * \param name Name of the target file
+ * \param mode Opening method : r+: reading, w+: writting ...
+ * \param t TypeRetour kind of return
+ *
+ * \return the target File open
+ */
 int deleteLast(list * l, void** data);
 
-
+/**
+ * \fn int insertAfterCurrent(list * l, void* data)
+ * \brief Function to open a target File
+ *
+ * \param name Name of the target file
+ * \param mode Opening method : r+: reading, w+: writting ...
+ * \param t TypeRetour kind of return
+ *
+ * \return the target File open
+ */
 
 int insertAfterCurrent(list * l, void* data);
+/**
+ * \fn int deleteCurrent(list * l, void** data)
+ * \brief Function to open a target File
+ *
+ * \param name Name of the target file
+ * \param mode Opening method : r+: reading, w+: writting ...
+ * \param t TypeRetour kind of return
+ *
+ * \return the target File open
+ */
 
-
-
-int insertBeforeCurrent(list * l, void* data);
 
 
 int deleteCurrent(list * l, void** data);
-
 
 
 
