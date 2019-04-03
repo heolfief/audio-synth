64f4e92020a2fc1a7a19c6e21fc2b58939bf8b56
Fief Heol <heol.fief@insa-rennes.fr>
2019-04-03 19:56:07 +0200

    Fix run script to work with preset files

--- run.sh
+++ run.sh
@@ -4,6 +4,7 @@ BLUE='\033[1;36m'
 NC='\033[0m' # No Color
 
 echo -e "${BLUE}\nExecuting program :${NC}"
-bin/audio_synth
+cd bin
+./audio_synth
 echo -e "Process finished with exit code "$?
 echo -e "${BLUE}\nDone${NC}"
