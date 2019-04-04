dd7e786424d60bc7a243f1df7362d0d2d6e1f1fb
Fief Heol <heol.fief@insa-rennes.fr>
2019-04-04 16:55:09 +0200

    Add amplitude LFO modulation

--- src/audio_fx/audio_fx.h
+++ src/audio_fx/audio_fx.h
@@ -20,10 +20,26 @@
  * \param buff the Audio_Buffer object to apply distortion to
  * \param buffer_length The size of the audio buffer (number of audio samples in the buffer)
  * \param dist_level the distortion level in percents
- * \param wet the balance bettwen dry(no effect) and wet(only effect) in percent
+ * \param wet the balance between dry(no effect) and wet(only effect) in percent
  *
  * \return 0 if everything went OK, -1 otherwise
  */
 int distortion(Audio_Buffer buff, Uint16 buffer_length, Uint8 dist_level, Uint8 wet);
+
+/**
+ * \fn int amp_mod(Audio_Buffer buff, Uint16 buffer_length, Uint32 sample_rate, double freq, double mod_level)
+ * \brief Function to apply amplitude modulation to an audio buffer
+ *
+ *  Volume is modulated by an LFO
+ *
+ * \param buff the Audio_Buffer object to apply distortion to
+ * \param buffer_length The size of the audio buffer (number of audio samples in the buffer)
+ * \param sample_rate The sample rate of the system
+ * \param freq the frequency of the modulation
+ * \param mod_level the distortion level in percents
+ *
+ * \return 0 if everything went OK, -1 otherwise
+ */
+int amp_mod(Audio_Buffer buff, Uint16 buffer_length, Uint32 sample_rate, double freq, double mod_level);
 
 #endif //AUDIO_SYNTH_AUDIO_FX_H
--- src/core/audio_core.c
+++ src/core/audio_core.c
@@ -60,11 +60,14 @@ int master_audio_fill_buffer()
 int master_effects()
 {
     double headroom = 0.8;
+
     // Leave headroom
     for(Uint16 sample = 0; sample < sys_param->audio_buffer_length; ++sample) master_audio[sample] = (Sint16)(headroom*master_audio[sample]);
 
     // Apply each effect to master audio buffer
-    if(distortion(master_audio,sys_param->audio_buffer_length,80,50))return -1;
+    if(distortion(master_audio,sys_param->audio_buffer_length,80,0))return -1;
+
+    if(amp_mod(master_audio,sys_param->audio_buffer_length,sys_param->sample_rate,15,50))return -1;
 
     return 0;
 }
