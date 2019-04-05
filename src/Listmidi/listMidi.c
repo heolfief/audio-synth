adfbe1bb0533c60f574c21158af0563966578349
Colombel Vincent <vcolombe@insa-rennes.fr>
2019-04-05 07:43:33 +0200

    improvement newnodelist

--- src/Listmidi/listMidi.c
+++ src/Listmidi/listMidi.c
@@ -8,7 +8,7 @@
 
 
 
-midiList * newList(void * ptr, midiList * next);
+midiList * newList(__uint16_t  midiNote, __uint16_t attack, __uint16_t  midiEvent, __uint32_t  delay, midiList * next);
 {
     midiList * new = (midiList*)malloc(sizeof(nodeList));  /*Allocation de l'espace mmoire*/
     if (new == NULL)
@@ -16,10 +16,12 @@ midiList * newList(void * ptr, midiList * next);
         fprintf(stderr, "Warning/error : allocation mmoire dynamique chou dans la fonction %s\n", __FUNCTION__);
         return NULL;		/*L'allocation  chou*/
     }
-    new->data = ptr;        /*Initialisation des poiteurs, data vers la donne  stocker, next vers la node suivante et next vers la suivante*/
-    new->next = n;
-
-
+    new->midiNote= midiNote;
+    new->midiEvent= midiEvent;
+    new->attack= attack;
+    new->delay = delay;
+    next->next = new;
+    new->next = NULL;
     return new;				/*Return du pointeur vers la node cre*/
 }
 
@@ -43,3 +45,19 @@ int oneElement(list * l)
     return (l->first != NULL && l->first == l->last);
 }
 
+
+int insertFirst(list * l, void* data)
+{
+
+    midiList* new = newList(data, l->first, NULL);
+    if (new == NULL)
+        return 0;
+    if (empty(l))
+        l->last = l->current = new;
+    else
+        l->first->previous = new;
+    l->first = new;
+
+    return 1;
+
+}
