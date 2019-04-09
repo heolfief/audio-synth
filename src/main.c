bad2b900feee5a0294cfeaa5b74ed2bfaddf205e
Fief Heol <heol.fief@insa-rennes.fr>
2019-04-10 01:07:14 +0200

    Change allocation method for audio core

--- src/main.c
+++ src/main.c
@@ -22,34 +22,33 @@ Uint16 mario_delay[7] = {80, 200, 250, 80, 250, 450, 450};
 int main(int argc, char *argv[])
 {
     SDL_AudioSpec as;
-    Core audio_core;
+    Core* audio_core;
 
-    audio_core.sys_param = alloc_sys_param();
-    if (audio_core.sys_param == NULL) exit(EXIT_FAILURE);
-
-    // Default parameters. If changed, memory allocation needs to be redone
-    audio_core.sys_param->sample_rate = 48000;
-    audio_core.sys_param->audio_buffer_length = 1024;
-
+    // Default parameters. If buffer_len changed, core memory allocation needs to be redone
+    int sample_rate = 48000;
+    int buffer_len = 1024;
 
     // Audio core init
-    if (init_core(&audio_core) != 0)
+    audio_core = alloc_core(buffer_len);
+    if (audio_core == NULL)
     {
         sys_print_error("Failed initializing audio core");
         exit(EXIT_FAILURE);
     }
 
-    if (load_preset("default.prst", audio_core.sys_param))
+    audio_core->sys_param->sample_rate = sample_rate;
+
+    if (load_preset("default.prst", audio_core->sys_param))
     {
         sys_print_error("Failed loading preset");
         exit(EXIT_FAILURE);
     }
 
-    save_preset("save_test.prst", audio_core.sys_param);
+    save_preset("save_test.prst", audio_core->sys_param);
 
 #ifndef VALGRIND
 
-    set_audio_spec(&as, &audio_core);
+    set_audio_spec(&as, audio_core);
 
     // SDL initialisations
     if (SDL_Init(SDL_INIT_EVERYTHING) < 0)
@@ -65,24 +64,26 @@ int main(int argc, char *argv[])
     }
 
     // Each time oscillator parameters changed, this function needs to be called
-    copy_osc_sys_param_to_notes_osc(audio_core.sys_param, audio_core.note_array);
+    if (copy_osc_sys_param_to_notes_osc(audio_core->sys_param, audio_core->note_array))return -1;
 
+    // Each time a filter parameter is changed, this function needs to be called
+    if (compute_filter_coeffs(audio_core->sys_param->filter_param, audio_core->sys_param->sample_rate, audio_core->effect_core->filter_state))return -1;
 
     // Hard coded note data, will be changed by reading MIDI files in the future
-    audio_core.note_array[0]->freq = 440;
-    audio_core.note_array[0]->onoff = OFF;
-    audio_core.note_array[0]->velocity_amp = 1;
-    audio_core.note_array[0]->master_onoff = OFF;
+    audio_core->note_array[0]->freq = 440;
+    audio_core->note_array[0]->onoff = OFF;
+    audio_core->note_array[0]->velocity_amp = 1;
+    audio_core->note_array[0]->master_onoff = OFF;
 
     SDL_PauseAudio(0);                      // Play audio (pause = off)
 
 
     for (int i = 0; i < 7; ++i)
     {
-        audio_core.note_array[0]->freq = mario_freq[i];
-        note_on(audio_core.note_array[0]);
+        audio_core->note_array[0]->freq = mario_freq[i];
+        note_on(audio_core->note_array[0]);
         SDL_Delay(100);
-        note_off(audio_core.note_array[0]);
+        note_off(audio_core->note_array[0]);
         SDL_Delay(mario_delay[i]);
     }
 
@@ -94,8 +95,7 @@ int main(int argc, char *argv[])
 #endif
 
     // Free all the data
-    quit_core(&audio_core);
-    free_sys_param(audio_core.sys_param);
+    free_core(audio_core);
 
     return 0;
 }
